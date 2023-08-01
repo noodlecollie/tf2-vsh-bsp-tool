a7949a35b62d4f0e13ea03a42404c95bdac596a7
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-08-01 18:07:50 +0100

    Added ability to patch cubemap material files

--- scripts/file_mgmt.py
+++ scripts/file_mgmt.py
@@ -10,6 +10,8 @@ class PakFile:
 		self.in_pak_path = out_path if disk_path is None else ""
 		self.in_disk_path = disk_path if disk_path is not None else ""
 		self.merge_function = None
+		self.patch_function = None
+		self.patch_replacements = {}
 
 	def basename_matches_suffix(self, map_name: str, suffix: str):
 		basename = os.path.splitext(os.path.basename(self.out_pak_path))[0]
@@ -37,8 +39,26 @@ class PakFile:
 	def can_merge(self):
 		return self.merge_function is not None
 
+	def can_patch(self):
+		return self.patch_function is not None
+
 def __makepath(path):
 	return path.replace("/", os.path.sep)
+
+def __read_from_disk(path: str):
+	if not os.path.isfile(path):
+		return bytes()
+
+	with open(path, "rb") as infile:
+		return infile.read()
+
+def __read_item(pakzip_in, item: PakFile):
+	if item.in_disk_path:
+		return __read_from_disk(item.in_disk_path)
+	elif item.in_pak_path:
+		return pak.get_file_data(pakzip_in, item.in_pak_path)
+	else:
+		return AssertionError(f"File {item.out_pak_path} had no source path")
 
 def __create_multi_source_file(existing_file: PakFile, new_file: PakFile):
 	if existing_file.in_disk_path and new_file.in_disk_path:
@@ -57,11 +77,11 @@ def __create_multi_source_file(existing_file: PakFile, new_file: PakFile):
 	if not existing_file.in_pak_path:
 		existing_file.in_pak_path = new_file.in_pak_path
 
-def merge_level_sounds_txt_data(existing_data: bytes, new_data: bytes):
+def __merge_level_sounds_txt_data(existing_data: bytes, new_data: bytes):
 	# TODO: This needs to be better. We need to remove duplicated objects after concatenation!
 	return existing_data + b'\n' + new_data
 
-def merge_particles_txt_data(existing_data: bytes, new_data: bytes):
+def __merge_particles_txt_data(existing_data: bytes, new_data: bytes):
 	# For particles, we need to merge the two particles_manifest objects together.
 
 	if not existing_data:
@@ -104,6 +124,24 @@ def merge_particles_txt_data(existing_data: bytes, new_data: bytes):
 
 	return b"particles_manifest\n" + keyvalues.serialise_single_depth_properties(combined_entries, indent=1) + b"\n"
 
+def __patch_cubemap_vmt_data(item: PakFile, data: bytes):
+	lines = data.split(b'\n')
+
+	for index in range(0, len(lines)):
+		line = lines[index]
+
+		for key in item.patch_replacements.keys():
+			value = item.patch_replacements[key]
+
+			key_bytes = b'"' + key.encode("latin-1") + b'"'
+			find_bytes = value[0].encode("latin-1")
+			replace_bytes = value[1].encode("latin-1")
+
+			if line.find(key_bytes) >= 0:
+				lines[index] = line.replace(find_bytes, replace_bytes)
+
+	return b'\n'.join(lines)
+
 def find_content_files_on_disk(content_path: str):
 	out_list = []
 
@@ -130,7 +168,7 @@ def resolve_names(file_list, old_map_name, new_map_name):
 	particles_path_prefix = __makepath("particles")
 
 	# Catches files with <number>_<number>_<number> at the end of their name
-	re_cubemap = re.compile(r".+-?\d+_-?\d+_-?\d+$")
+	re_cubemap = re.compile(r".+-?\d+_-?\d+_-?\d+(\.hdr)?$")
 
 	encountered_files = {}
 
@@ -145,24 +183,30 @@ def resolve_names(file_list, old_map_name, new_map_name):
 			if dirname.startswith(map_manifest_path_prefix):
 				if item.basename_matches_suffix(old_map_name, "_level_sounds"):
 					new_path = os.path.join(dirname, f"{new_map_name}_level_sounds{extension}")
-					item.merge_function = merge_level_sounds_txt_data
+					item.merge_function = __merge_level_sounds_txt_data
 				elif item.basename_matches_suffix(old_map_name, "_particles"):
 					new_path = os.path.join(dirname, f"{new_map_name}_particles{extension}")
-					item.merge_function = merge_particles_txt_data
+					item.merge_function = __merge_particles_txt_data
 			# particles/
 			elif dirname.startswith(particles_path_prefix):
 				if item.basename_matches_suffix(old_map_name, "_manifest"):
 					new_path = os.path.join(dirname, f"{new_map_name}_manifest{extension}")
-					item.merge_function = merge_particles_txt_data
+					item.merge_function = __merge_particles_txt_data
 			# scripts/
 			elif dirname.startswith(soundscape_path_prefix):
 				if item.basename_matches_prefix(old_map_name, "soundscapes_"):
 					new_path = os.path.join(dirname, f"soundscapes_{new_map_name}{extension}")
-		elif extension == ".vtf":
+		elif extension in [".vtf", ".vmt"]:
 			# materials/maps/map_name/
 			if dirname.startswith(cubemap_path_prefix):
 				if re_cubemap.match(basename):
 					new_path = new_cubemap_path_prefix + item.out_pak_path[len(cubemap_path_prefix):]
+
+					if extension == ".vmt":
+						item.patch_function = __patch_cubemap_vmt_data
+
+						# Replace the map name in the expected paths only (ie. match the surrounding separators too)
+						item.patch_replacements = {"$envmap": (f"/{old_map_name}/", f"/{new_map_name}/")}
 
 		if new_path is not None:
 			item.out_pak_path = new_path
@@ -179,27 +223,34 @@ def add_files_to_pak(file_list, pakdata_in, pakdata_out):
 		with zipfile.ZipFile(pakdata_in, mode="r") as pakzip_in:
 			for item in file_list:
 				if item.can_merge() and item.is_multi_source():
-					# Will be an empty byte array if not found.
+					# These will provide empty byte arrays if the files are not found.
 					existing_data = pak.get_file_data(pakzip_in, item.in_pak_path)
-
-					new_data = bytes()
-
-					if os.path.isfile(item.in_disk_path):
-						with open(item.in_disk_path, "rb") as infile:
-							new_data = infile.read()
+					new_data = __read_from_disk(item.in_disk_path)
 
 					print(f"Merging {item.out_pak_path}:")
 					print(f"  + {item.in_pak_path}")
 					print(f"  + {item.in_disk_path}")
-					item.merge_function(existing_data, new_data)
-				else:
-					if item.in_disk_path:
-						print(f"Embedding {item.out_pak_path}")
-						pak.try_write_disk_file(pakzip_out, item.in_disk_path, item.out_pak_path)
-					else:
-						if item.out_pak_path != item.in_pak_path:
-							print(f"Renaming {item.in_pak_path} -> {item.out_pak_path}")
-						else:
-							print(f"Retaining {item.out_pak_path}")
 
-						pak.try_write_data(pakzip_out, item.out_pak_path, pak.get_file_data(pakzip_in, item.in_pak_path))
+					pak.try_write_data(pakzip_out, item.out_pak_path, item.merge_function(existing_data, new_data))
+
+					continue
+
+				data = __read_item(pakzip_in, item)
+
+				if item.can_patch():
+					if item.in_disk_path or item.out_pak_path == item.in_pak_path:
+						print(f"Patching {item.out_pak_path}")
+					else:
+						print(f"Patching and renaming {item.in_pak_path} -> {item.out_pak_path}")
+				elif item.in_disk_path:
+					print(f"Embedding {item.out_pak_path}")
+				else:
+					if item.out_pak_path != item.in_pak_path:
+						print(f"Renaming {item.in_pak_path} -> {item.out_pak_path}")
+					else:
+						print(f"Retaining {item.out_pak_path}")
+
+				if item.can_patch():
+					data = item.patch_function(item, data)
+
+				pak.try_write_data(pakzip_out, item.out_pak_path, data)
